958a0e0548b14c6217e8a3ab292c0f29ebb8ce7d
Soumya <soumya@soumyadsanyal.com>
2016-06-27 17:08:29 -0400

    ..

--- soumya.c
+++ soumya.c
@@ -20,31 +20,17 @@ char *readline(char *content, int line_size){
 	while ((c=getchar()) != '\n'){
 		*(return_value++) = c;
 		position++;
-//		printf("position is %d and line_size is %d\n", position, line_size);
 		if (position>=line_size){
 			line_size=line_size*2;
-// this part (dynamic resizing) isn't working yet, NEED TO fix
- //			printf("old address is %d", (int) content);
 			char *try = realloc(content, line_size);
  			if (try ==NULL){
 				return NULL;
 }
 			else{content=try;}
-			printf("new address is %d", (int) content);
-			printf("\n\nline size is %d\n\n", line_size);
 			return_value = content+position;
-//			printf("\n\ncontent is located at %d, return_value is located at %d\n\n", (int) content, (int) return_value); 
 }
 }
 	*return_value='\0';
-//	printf("\nfinal starting and ending addresses are %d and %d, with an offset of %d\n", (int) content, (int) return_value, (int) (return_value - content));
-	char *p = content;
-	while (p<=return_value){	
-//	printf("(character is %c, located at %d, start of sentence is %d, end of sentence is %d\n) ", *p, (int) p, (int) content, (int) return_value);
-	p++;
-}
-	//return_value++;
-	//content=return_value;
 	return content;
 }
 
