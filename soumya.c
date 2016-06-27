44cf666420801e8e5748b073fa5696f1ad7f78e3
Soumya <soumya@soumyadsanyal.com>
2016-06-27 17:07:58 -0400

    .

--- soumya.c
+++ soumya.c
@@ -4,45 +4,56 @@
 #include <string.h>
 
 int length(char *s){
-	int count = 0;
 	char *p = s;
 	printf("%s", p);
 	while(*(p) != '\0'){
-		count++;
 		p++;
 }
 	printf("\n");
-	return count;
+	return (int) (p - s);
 }
 
-int readline(char *content, int line_size){
+char *readline(char *content, int line_size){
 	char *return_value = content;
 	char c;
 	int position=0;
 	while ((c=getchar()) != '\n'){
 		*(return_value++) = c;
 		position++;
+//		printf("position is %d and line_size is %d\n", position, line_size);
 		if (position>=line_size){
-			line_size+=line_size;
+			line_size=line_size*2;
 // this part (dynamic resizing) isn't working yet, NEED TO fix
-			return_value=realloc(return_value, line_size);
-			if (return_value==NULL){
-				return -1;
+ //			printf("old address is %d", (int) content);
+			char *try = realloc(content, line_size);
+ 			if (try ==NULL){
+				return NULL;
 }
-			return_value = return_value+position;
+			else{content=try;}
+			printf("new address is %d", (int) content);
+			printf("\n\nline size is %d\n\n", line_size);
+			return_value = content+position;
+//			printf("\n\ncontent is located at %d, return_value is located at %d\n\n", (int) content, (int) return_value); 
 }
 }
 	*return_value='\0';
+//	printf("\nfinal starting and ending addresses are %d and %d, with an offset of %d\n", (int) content, (int) return_value, (int) (return_value - content));
+	char *p = content;
+	while (p<=return_value){	
+//	printf("(character is %c, located at %d, start of sentence is %d, end of sentence is %d\n) ", *p, (int) p, (int) content, (int) return_value);
+	p++;
+}
 	//return_value++;
 	//content=return_value;
-	return 0;
+	return content;
 }
 
 int main(){
-	int line_size = 128;
+	int line_size = 10;
 	char *input = malloc(line_size); 
 	printf("Enter a string, and I'll compute the length: \n");
-	readline(input, line_size);
+	input = readline(input, line_size);
+	printf("\nthe returned value from the readline function is %s.\nThe locations of the start and end of the string are %d and placeholder\n", input, (int) input);
 	printf("The length is %d and the string is %s\n", length(input), input);
 	}
 
