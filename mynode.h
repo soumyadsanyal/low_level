0f97945b6444c7c2194eddc884728452688f449d
Soumya <soumya@soumyadsanyal.com>
2016-07-06 23:42:29 -0400

    .

--- mynode.h
+++ mynode.h
@@ -6,3 +6,26 @@ struct node {
 typedef struct node node;
 
 
+int enumerate_list(node *head){
+	node *p = head;
+	printf("Node contains %d, located at %p, points to %p\n", (*p).data, p, (*p).next);
+	while(((*p).next)!=NULL) {
+	p=(*p).next;
+	printf("Node contains %d, located at %p, points to %p\n", (*p).data, p, (*p).next);
+}
+	return 0;
+}
+
+node *make_node(void){
+	node *n = malloc(sizeof(node));
+	printf("Enter data for node: ");
+	scanf("%d", &((*n).data));
+	(*n).next=NULL;
+	return n;
+}
+
+int connect_nodes(node *first, node *second){
+	(*first).next = second;
+	return 0;
+}
+
--- stacks.c
+++ stacks.c
@@ -7,29 +7,6 @@
 
 
 
-int enumerate_list(node *head){
-	node *p = head;
-	printf("Node contains %d, located at %p, points to %p\n", (*p).data, p, (*p).next);
-	while(((*p).next)!=NULL) {
-	p=(*p).next;
-	printf("Node contains %d, located at %p, points to %p\n", (*p).data, p, (*p).next);
-}
-	return 0;
-}
-
-node *make_node(void){
-	node *n = malloc(sizeof(node));
-	printf("Enter data for node: ");
-	scanf("%d", &((*n).data));
-	(*n).next=NULL;
-	return n;
-}
-
-int connect_nodes(node *first, node *second){
-	(*first).next = second;
-	return 0;
-}
-
 int main(void){
 	
 	int another = 1;
